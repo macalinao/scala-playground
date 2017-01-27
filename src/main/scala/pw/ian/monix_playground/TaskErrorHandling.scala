f777d26e7ca5ee1de57ff8c5df4d697606cab9b1
Ian Macalinao <me@ian.pw>
2017-01-26 20:34:22 -0600

    test task error handling

---
+++ src/main/scala/pw/ian/monix_playground/TaskErrorHandling.scala
@@ -0,0 +1,31 @@
+package pw.ian.monix_playground
+
+import monix.eval.Task
+import monix.execution.Scheduler
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+
+object TaskErrorHandling {
+
+  def main(args: Array[String]): Unit = {
+    implicit val scheduler = Scheduler.computation(20)
+    val task = Task.eval {
+      throw new Exception("fuck")
+    }.onErrorHandle { _ =>
+      println("we good")
+    }.map { _ =>
+      println("fo real tho")
+    }
+    Await.result(task.runAsync, Duration.Inf)
+
+    val task2 = Task.eval {
+      throw new Exception("fuck")
+    }.map { _ =>
+      println("should not print")
+    }
+    Await.result(task2.runAsync, Duration.Inf)
+  }
+
+
+}
