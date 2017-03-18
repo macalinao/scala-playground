61fac1ffda16f53e81ddd5a40b5a49aabc395fa4
Ian Macalinao <me@ian.pw>
2017-03-18 16:39:16 -0500

    serial parallel

--- src/main/scala/pw/ian/monix_playground/DeferFuture.scala
+++ src/main/scala/pw/ian/monix_playground/DeferFuture.scala
@@ -15,7 +15,6 @@ object DeferFuture {
   def main(args: Array[String]): Unit = {
     implicit val scheduler = Scheduler.computation(20)
 
-    // These tasks will execute serially.
     val tasks = List.fill(100)(()).map { _ =>
       Task.deferFuture {
         Future {
@@ -25,7 +24,12 @@ object DeferFuture {
       }
     }
 
+    // These tasks will execute serially.
     Await.result(tasks.sequence.runAsync, Duration.Inf)
+
+    // To run in parallel, use this:
+    // Await.result(Task.gather(tasks).runAsync, Duration.Inf)
+    // https://monix.io/docs/2x/tutorials/parallelism.html#observablemergemap
   }
 
 }
